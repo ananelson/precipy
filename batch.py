7d0388273328cd82be86323f654753698e45c349
Ana Nelson <ana@ananelson.com>
2019-09-22 15:57:11 -0700

    make Batch class

---
+++ batch.py
@@ -0,0 +1,79 @@
+from google.api_core.exceptions import NotFound
+from jinja2 import Environment, select_autoescape
+from google.cloud import storage
+from identifiers import cache_filename_for_fn
+from identifiers import hash_for_fn
+from pathlib import Path
+import analytics
+import json
+import tempfile
+
+DEFAULT_BUCKET_NAME = "artifacts"
+
+class Batch(object):
+    def __init__(self, request):
+        self.request = request
+        self.info = request.get_json()
+        self.bucket_name = self.info.get('bucket_name', DEFAULT_BUCKET_NAME)
+        self.init_storage()
+        self.jinja_env = Environment(
+            autoescape=select_autoescape(['html', 'xml'])
+            )
+        self.template_data = {}
+
+    def init_storage(self):
+        self.storage_client = storage.Client()
+        try:
+            self.storage_bucket = self.storage_client.get_bucket(self.bucket_name)
+        except NotFound:
+            self.storage_bucket = self.storage_client.create_bucket(self.bucket_name)
+
+    def load_if_cached(self, h):
+        hashed_filename = cache_filename_for_fn(h)
+        blob = self.storage_bucket.get_blob(hashed_filename)
+    
+        if blob is None:
+            return
+    
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            fn = Path(tmpdirname) / hashed_filename
+            print(fn)
+            blob.download_to_filename(fn)
+    
+            with open(fn, 'r') as ff:
+                json.load(ff)
+
+    def save_to_cache(self, h, data):
+        hashed_filename = cache_filename_for_fn(h)
+        blob = self.storage_bucket.blob(hashed_filename)
+    
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            fn = Path(tmpdirname) / hashed_filename
+            with open(fn, 'w') as ff:
+                json.dump(data, ff)
+    
+            blob.upload_from_filename(str(fn))
+
+    def generate_analytics(self):
+        for function_name, kwargs in self.info.get('analytics', []):
+            # get function object from function name
+            fn = getattr(analytics, function_name)
+            # TODO generalize module name beyond hard-coded 'analytics'
+
+            h = hash_for_fn(fn, kwargs)
+            data = self.load_if_cached(h)
+
+            if data is None:
+                # TODO add registered output files + hashes to data
+                data = {}
+                data['function_output']  = fn(**kwargs)
+                self.save_to_cache(h, data)
+
+            self.template_data[function_name] = data
+
+    def template_text(self):
+        return self.info.get("template")
+
+    def render_template(self):
+        template = self.jinja_env.from_string(self.template_text())
+        return template.render(self.template_data)
--- main.py
+++ main.py
@@ -1,47 +1,6 @@
-from identifiers import hash_for_fn
-from jinja2 import Environment, select_autoescape
-from storage import load_if_cached
-from storage import save_to_cache
-import analytics
-
-# declare 'heavy' resources as globals
-jinja_env = None
-
-def init_jinja_env():
-    return Environment(
-            autoescape=select_autoescape(['html', 'xml'])
-            )
+from batch import Batch
 
 def render(request):
-    # declare and lazy load globals
-    global jinja_env
-    if jinja_env is None:
-        jinja_env = init_jinja_env()
-    # https://cloud.google.com/functions/docs/bestpractices/tips
-
-    request_json = request.get_json()
-    bucket_name = request_json.get('bucket_name')
-
-    # process data sources 
-    for function_name, kwargs in request_json['analytics']:
-        # get function object from function name
-        # TODO generalize module name
-        fn = getattr(analytics, function_name)
-
-        h = hash_for_fn(fn, kwargs)
-        data = load_if_cached(h, bucket_name)
-
-        if data is None:
-            # TODO add registered output files + hashes to data
-            data = {}
-            data['function_output']  = fn(**kwargs)
-            save_to_cache(h, data, bucket_name)
-
-    # render the template
-    if request.args and 'template' in request.args:
-        template_text = request.template
-    elif request_json and 'template' in request_json:
-        template_text = request_json['template']
-
-    template = jinja_env.from_string(template_text)
-    return template.render({'foo' : 200})
+    batch = Batch(request)
+    batch.generate_analytics()
+    return batch.render_template()
