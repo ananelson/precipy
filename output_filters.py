cc652c637fb555af5f7f3d8cc6ad6d520e9524dd
Ana Nelson <ana@ananelson.com>
2019-10-24 08:19:58 -0700

    revert pandoc filter

--- output_filters.py
+++ output_filters.py
@@ -1,8 +1,10 @@
-import pypandoc
-import os
+import subprocess
 
-def do_pandoc(batch, input_filepath, output_filepath, output_ext, args):
-    curdir = os.getcwd()
-    os.chdir(batch.workdir.name)
-    pypandoc.convert_file(input_filepath, output_ext, outputfile=output_filepath)
-    os.chdir(curdir)
+def do_pandoc(batch, input_filepath, output_filepath, output_ext, filter_args):
+    p = subprocess.run(['which', 'pandoc'], capture_output=True)
+    print(p.stderr)
+    print(p.stdout)
+
+    p = subprocess.run(['/opt/local/bin/pandoc', input_filepath, '-o', output_filepath], cwd=batch.workdir.name, capture_output=True, check=True)
+    print(p.stderr)
+    print(p.stdout)
