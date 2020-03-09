ab3f1398de8ba757b5228637c62abd26a5797d6a
Ana Nelson <ana@ananelson.com>
2020-03-09 16:46:28 -0700

    Add support for weasyprint

--- precipy/output_filters.py
+++ precipy/output_filters.py
@@ -1,5 +1,6 @@
 import subprocess
 from xhtml2pdf import pisa
+from weasyprint import HTML
 import markdown
 import os
 
@@ -16,6 +17,9 @@ def do_xhtml2pdf(batch, input_filepath, output_filepath, output_ext, filter_args
     with open(input_filepath, 'r') as i_f:
         with open(output_filepath, 'wb') as o_f:
             pisa.CreatePDF(i_f.read(), dest=o_f)
+
+def do_weasyprint(batch, input_filepath, output_filepath, output_ext, filter_args):
+    HTML(input_filepath).write_pdf(output_filepath)
 
 def do_pandoc(batch, input_filepath, output_filepath, output_ext, filter_args):
     subprocess.run(['/opt/local/bin/pandoc', input_filepath, '-o', output_filepath], cwd=batch.cache_dir.name, capture_output=True, check=True)
