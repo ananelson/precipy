0f473a0e445deccc4f1007fc310b0860c4b0f842
Ana Nelson <ana@ananelson.com>
2020-03-23 21:55:22 -0700

    Remove unused dependencies

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="precipy",
-    version="0.2.2",
+    version="0.2.3",
     author="Ana Nelson",
     author_email="ana@ananelson.com",
     url="https://github.com/ananelson/precipy",
     packages=setuptools.find_packages(),
     install_requires = [
-        "google-cloud-storage",
         "jinja2",
         "markdown",
+        "pygments"
         ],
     classifiers=[
         "Programming Language :: Python :: 3",
